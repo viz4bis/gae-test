d6ad7f9d15d4c9f12880a6805953e6219d13c42d
test4webapp <test4webapp@gmail.com>
2014-07-09 08:24:44 +0900

    implemented view

--- main.go
+++ main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
-  "net/http"
-  "net/url"
-  //"html/template"
-  "github.com/PuerkitoBio/goquery"
-  )
+	"appengine"
+	"appengine/urlfetch"
+  //"database/sql"
+	"time"
+	"encoding/json"
+	"fmt"
+	//"io"
+	//"io/ioutil"
+	"code.google.com/p/go-html-transform/h5"
+	"code.google.com/p/go-html-transform/html/transform"
+	"code.google.com/p/go.net/html"
+	"net/http"
+	"net/url"
+	"strconv"
+  //_ "github.com/iitaku/go-yql"
+	//"code.google.com/p/go-html-transform/css/selector"
+	//"html/template"
+	//"github.com/PuerkitoBio/goquery"
+)
 
 func init() {
-  http.HandleFunc("/", root)
-  http.HandleFunc("/connect-weibo", connect_weibo)
-  http.HandleFunc("/view", view)
+	http.HandleFunc("/", root)
+	http.HandleFunc("/connect-weibo", connect_weibo)
+	http.HandleFunc("/view", view)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprint(w, root_html)
+	fmt.Fprint(w, root_html)
 }
 
 const root_html = `
@@ -34,37 +46,126 @@ const root_html = `
 `
 
 func connect_weibo(w http.ResponseWriter, r *http.Request) {
-  resp, err := http.PostForm(
-    "https://api.weibo.com/oauth2/access_token",
-    url.Values{"client_id":{"2909741328"},
-    "client_secret":{"61d75f2cceb35c8d95dd7185afb1dd5c"},
-    "grant_type":{"authorization_code"},
-    "code":{r.FormValue("code")},
-    "redirect_uri":{"http://ambient-depth-612.appspot.com/view"}})
+	c := appengine.NewContext(r)
+	client := urlfetch.Client(c)
 
-  body, err := ioutil.ReadAll(resp.Body)
-  if (resp.StatusCode != http.StatusOK) {
-    fmt.Fprint(w, "fatal1\n")
-    return
-  }
+	resp, err := client.PostForm(
+		"https://api.weibo.com/oauth2/access_token",
+		url.Values{"client_id": {"2909741328"},
+			"client_secret": {"61d75f2cceb35c8d95dd7185afb1dd5c"},
+			"grant_type":    {"authorization_code"},
+			"code":          {r.URL.Query()["code"][0]},
+			"redirect_uri":  {"http://ambient-depth-612.appspot.com/connect-weibo"}})
 
-  if err !=nil {
-    fmt.Fprint(w, "fatal2\n")
-    return
-  }
+	if err != nil {
+		fmt.Fprintf(w, "err : %s\n", err.Error())
+		return
+	}
 
-  if (body[0] == 0) {
-    fmt.Fprint(w, "connected1\n")
-  } else {
-    fmt.Fprint(w, "connected2\n")
-  }
-
+	var kv map[string]interface{}
+	dec := json.NewDecoder(resp.Body)
+	dec.Decode(&kv)
+	if at, ok := kv["access_token"].(string); ok {
+		cookie := http.Cookie{Name: "access_token", Value: at}
+		http.SetCookie(w, &cookie)
+	}
+	http.Redirect(w, r, "/view", http.StatusMovedPermanently)
 }
 
 func view(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprint(w, "view")
+	c := appengine.NewContext(r)
+	client := urlfetch.Client(c)
+
+  // weibo
+	at, err := r.Cookie("access_token")
+	if err != nil {
+		fmt.Fprintf(w, "err : %s\n", err.Error())
+		return
+	}
+
+	resp, err := client.Get("https://api.weibo.com/2/search/suggestions/users.json?q=uniqlo&access_token=" + at.Value)
+	if err != nil {
+		fmt.Fprintf(w, "err : %s\n", err.Error())
+		return
+	}
+	var arr []interface{}
+	dec := json.NewDecoder(resp.Body)
+	dec.Decode(&arr)
+	var uid string
+	if len(arr) != 0 {
+		if kv, ok := arr[0].(map[string]interface{}); ok {
+			if v, ok := kv["uid"].(float64); ok {
+				uid = strconv.FormatFloat(v, 'f', 0, 64)
+			}
+		} else {
+			fmt.Fprintf(w, "err : %s\n", err.Error())
+			return
+		}
+	}
+
+	resp, err = client.Get("https://api.weibo.com/2/users/show.json?uid=" + uid + "&access_token=" + at.Value)
+	if err != nil {
+		fmt.Fprintf(w, "err : %s\n", err.Error())
+		return
+	}
+	var kv map[string]interface{}
+	dec = json.NewDecoder(resp.Body)
+	dec.Decode(&kv)
+	if v, ok := kv["followers_count"].(float64); ok {
+		fmt.Fprintf(w, "weibo follower : %v\n", uint64(v))
+	}
 
   // qq by scraping
-  doc, _ := goquery.NewDocument("e.t.qq.com")
-  fmt.Fprint(w, doc.Find("hoge"))
+	resp, err = client.Get("http://e.t.qq.com/uniqlochina")
+	if err != nil {
+		fmt.Fprintf(w, "err : %s\n", err.Error())
+		return
+	}
+
+	tree, err := h5.New(resp.Body)
+	if err != nil {
+		fmt.Fprintf(w, "err : %s\n", err.Error())
+		return
+	}
+
+	t := transform.New(tree)
+	var qq_follower uint64
+	t.Apply(func(node *html.Node) {
+		for _, v := range node.Attr {
+			if v.Val == "http://t.qq.com/uniqlochina/follower" {
+				if qq_follower, err = strconv.ParseUint(node.FirstChild.Data, 10, 64); err != nil {
+					qq_follower = 0
+				}
+			}
+		}
+	}, "a.text_count.co_c_tx")
+	fmt.Fprintf(w, "qq follower : %v\n", qq_follower)
+
+  // stock by yql
+  resp, err = client.Get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22FRCOY%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=")
+  if err != nil {
+    fmt.Fprintln(w, "%v\n", err)
+    return
+  }
+  var kv2 map[string]interface {}
+  dec = json.NewDecoder(resp.Body)
+  dec.Decode(&kv2)
+  var quote float64
+  if kv3, ok := kv2["query"].(map [string] interface{}); ok {
+    if kv4, ok := kv3["results"].(map [string] interface{}); ok {
+      if kv5, ok := kv4["quote"].(map [string] interface{}); ok {
+        if quote_s, ok := kv5["LastTradePriceOnly"].(string); ok {
+          if quote, err = strconv.ParseFloat(quote_s, 64); err != nil {
+            quote = 0
+          }
+        }
+      }
+    }
+  }
+  fmt.Fprintf(w, "quote : %v\n", quote)
+}
+
+func cron(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+  c.Infof("%v\n", time.Now())
 }
