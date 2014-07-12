7e252ee475ecb27a8f5d3c08d89e8539ac5a9971
test4webapp <test4webapp@gmail.com>
2014-07-12 22:30:22 +0900

    refactored

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"appengine"
+	"appengine/datastore"
 	"appengine/urlfetch"
+	//"errors"
   //"database/sql"
 	"time"
 	"encoding/json"
@@ -21,17 +23,35 @@ import (
 	//"github.com/PuerkitoBio/goquery"
 )
 
+type WeiboAccessToken struct {
+  Token string
+}
+
+type DBValue struct {
+  Weibo int64 
+  Qq    int64 
+  Stock float64 
+}
+
 func init() {
 	http.HandleFunc("/", root)
-	http.HandleFunc("/connect-weibo", connect_weibo)
-	http.HandleFunc("/view", view)
+	http.HandleFunc("/setup", setup)
+	http.HandleFunc("/fetch-and-store", fetch_and_store)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, root_html)
+  c := appengine.NewContext(r)
+  q := datastore.NewQuery("DBValue").Limit(10)
+  var values []DBValue
+  _, err := q.GetAll(c, &values)
+  if err != nil {
+    c.Errorf("failed to get values : %v\n", err)
+    return
+  }
+  fmt.Fprintf(w, "%v\n", values)
 }
 
-const root_html = `
+const setup_html = `
  <html>
  <body>
  <div>
@@ -39,11 +59,15 @@ const root_html = `
   <input type="hidden" name="client_id" value="2909741328">
   <input type="hidden" name="redirect_uri" value="http://ambient-depth-612.appspot.com/connect-weibo">
   <input type="hidden" name="response_type" value="code">
-  <button>View</button>
+  <button>setup</button>
   </form></div>
  </body>
  </html>
 `
+
+func setup(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprint(w, setup_html)
+}
 
 func connect_weibo(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
@@ -58,32 +82,56 @@ func connect_weibo(w http.ResponseWriter, r *http.Request) {
 			"redirect_uri":  {"http://ambient-depth-612.appspot.com/connect-weibo"}})
 
 	if err != nil {
-		fmt.Fprintf(w, "err : %s\n", err.Error())
-		return
+    c.Errorf("%v\n", err)
+    return
 	}
 
 	var kv map[string]interface{}
 	dec := json.NewDecoder(resp.Body)
 	dec.Decode(&kv)
 	if at, ok := kv["access_token"].(string); ok {
-		cookie := http.Cookie{Name: "access_token", Value: at}
-		http.SetCookie(w, &cookie)
+    entity := WeiboAccessToken {
+      Token : at,
+    }
+    if key, err := datastore.Put(c, datastore.NewIncompleteKey(c, "WeiboAccessToken", nil), &entity); err != nil {
+      c.Errorf("failed to put : %v\n", err)
+      return
+    } else {
+      c.Infof("encode : %v\n", key.Encode())
+    }
 	}
-	http.Redirect(w, r, "/view", http.StatusMovedPermanently)
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
 }
 
-func view(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
+func lookup_weibo_access_token(r *http.Request) (string, error) {
+  c := appengine.NewContext(r)
+  if key, err := datastore.DecodeKey("ahNzfmFtYmllbnQtZGVwdGgtNjEych0LEhBXZWlib0FjY2Vzc1Rva2VuGICAgIC8oYIKDA"); err != nil {
+    return "", err
+  } else {
+    var entity WeiboAccessToken
+    if err := datastore.Get(c, key, &entity); err != nil {
+      return "", err
+    }
+    return entity.Token, nil
+  }
+}
+
+func fetch_and_store(w http.ResponseWriter, r *http.Request) {
+
+  value := DBValue { Weibo : 0, Qq : 0, Stock : 0.0 }
+
+  c := appengine.NewContext(r)
 	client := urlfetch.Client(c)
 
-  // weibo
-	at, err := r.Cookie("access_token")
-	if err != nil {
-		fmt.Fprintf(w, "err : %s\n", err.Error())
-		return
-	}
+  c.Infof("fetch-and-store")
 
-	resp, err := client.Get("https://api.weibo.com/2/search/suggestions/users.json?q=uniqlo&access_token=" + at.Value)
+  // weibo
+  var err error
+  var token string 
+  if token, err = lookup_weibo_access_token(r); err != nil {
+    c.Errorf("failed to get token : %v\n", err)
+  }
+	resp, err := client.Get("https://api.weibo.com/2/search/suggestions/users.json?q=uniqlo&access_token=" + token)
 	if err != nil {
 		fmt.Fprintf(w, "err : %s\n", err.Error())
 		return
@@ -103,7 +151,7 @@ func view(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	resp, err = client.Get("https://api.weibo.com/2/users/show.json?uid=" + uid + "&access_token=" + at.Value)
+	resp, err = client.Get("https://api.weibo.com/2/users/show.json?uid=" + uid + "&access_token=" + token)
 	if err != nil {
 		fmt.Fprintf(w, "err : %s\n", err.Error())
 		return
@@ -112,60 +160,70 @@ func view(w http.ResponseWriter, r *http.Request) {
 	dec = json.NewDecoder(resp.Body)
 	dec.Decode(&kv)
 	if v, ok := kv["followers_count"].(float64); ok {
-		fmt.Fprintf(w, "weibo follower : %v\n", uint64(v))
+		value.Weibo =  int64(v)
 	}
 
   // qq by scraping
 	resp, err = client.Get("http://e.t.qq.com/uniqlochina")
 	if err != nil {
-		fmt.Fprintf(w, "err : %s\n", err.Error())
+    c.Errorf("qq")
 		return
 	}
 
 	tree, err := h5.New(resp.Body)
 	if err != nil {
-		fmt.Fprintf(w, "err : %s\n", err.Error())
-		return
+    c.Errorf("qq")
+    return
 	}
 
 	t := transform.New(tree)
-	var qq_follower uint64
 	t.Apply(func(node *html.Node) {
 		for _, v := range node.Attr {
 			if v.Val == "http://t.qq.com/uniqlochina/follower" {
-				if qq_follower, err = strconv.ParseUint(node.FirstChild.Data, 10, 64); err != nil {
-					qq_follower = 0
+				if value.Qq, err = strconv.ParseInt(node.FirstChild.Data, 10, 64); err != nil {
+          c.Errorf("qq")
+          return
 				}
 			}
 		}
 	}, "a.text_count.co_c_tx")
-	fmt.Fprintf(w, "qq follower : %v\n", qq_follower)
 
   // stock by yql
   resp, err = client.Get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22FRCOY%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=")
   if err != nil {
-    fmt.Fprintln(w, "%v\n", err)
+    c.Errorf("stock")
     return
   }
   var kv2 map[string]interface {}
   dec = json.NewDecoder(resp.Body)
   dec.Decode(&kv2)
-  var quote float64
   if kv3, ok := kv2["query"].(map [string] interface{}); ok {
     if kv4, ok := kv3["results"].(map [string] interface{}); ok {
       if kv5, ok := kv4["quote"].(map [string] interface{}); ok {
         if quote_s, ok := kv5["LastTradePriceOnly"].(string); ok {
-          if quote, err = strconv.ParseFloat(quote_s, 64); err != nil {
-            quote = 0
+          if value.Stock, err = strconv.ParseFloat(quote_s, 64); err != nil {
+            c.Errorf("stock")
+            return
           }
+        } else {
+          c.Errorf("stock")
+          return
         }
+      } else {
+        c.Errorf("stock")
+        return
       }
+    } else {
+      c.Errorf("stock")
+      return
     }
+  } else {
+    c.Errorf("stock")
+    return
   }
-  fmt.Fprintf(w, "quote : %v\n", quote)
-}
 
-func cron(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-  c.Infof("%v\n", time.Now())
+  if _, err := datastore.Put(c, datastore.NewKey(c, "DBValue", "", time.Now().Unix(), nil), &value); err != nil {
+    c.Errorf("failed to put data : %v", err);
+    return
+  }
 }
